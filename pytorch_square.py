2cb492699010835097b9adefea4be134d0474bef
Janni Tselekoglou <Janni@Jannis-Air.fritz.box>
2022-11-18 15:23:35 +0100

    updated pytorch_square.py

--- pytorch_square.py
+++ pytorch_square.py
@@ -1,52 +1,63 @@
 import torch
 import torch.nn as nn
-
+import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 
 #Creating the basick modell
 
-model = nn.Sequential(
-  nn.Linear(1,10),
-  nn.ReLU(),
-  nn.Linear(10,10),
-  nn.ReLU(),
-  nn.Linear(10,10),
-  nn.ReLU(),
-  nn.Linear(10,10),
-  nn.ReLU(),
-  nn.Linear(10,10),
-  nn.ReLU(),
-  nn.Linear(10,1)
-)
 
-
-
+class NeuralNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.linear_stack =  nn.Sequential(
+         nn.Linear(1,15),
+         nn.ReLU(),
+         nn.Linear(15,15),
+         nn.ReLU(),
+         nn.Linear(15,15),
+         nn.ReLU(),
+         nn.Linear(15,1),
+         )
+    def forward(self,x):
+        logits = self.linear_stack(x)
+        return logits
 
 #print(model(torch.rand(10,1)))
 
 
-a = sum(model(torch.rand(10,1)))
-
+model = NeuralNetwork()
 
 #use optimizer which will deal with bias and gradients 
 
+#train_set
 batch_size = 10000
-
 X = torch.rand(batch_size,1)
 Y = X**2
 
-optim = torch.optim.SGD(model.parameters(), lr = 0.01)
+lr = 0.01
+optim = torch.optim.Adam(model.parameters(), lr=lr)
 
-epochs = 1000
-for i in range(epochs):
-  out = model(X)
-  loss = sum((out-Y)**2/batch_size)
-  print(loss)
-  loss.backward()
-  optim.step() #diesen schritt noch besser versethen 
-  optim.zero_grad() #diesen schritt noch besser versethen 
+loss = torch.nn.MSELoss()
+n_iters = 2000
 
 
+for epochs in range(n_iters):
+    #forward pass and loss
+    out = model(X)
+    l = loss(out,Y)
+    #backward pass
+    l.backward()
+    #update weights
+    optim.step()
+    optim.zero_grad()
 
-X = torch.rand(2,1)
-print(X**2)
-print(model(X))
+    if epochs % 100 == 0:
+        print(f" {epochs+1}, l={l.item()}")
+
+
+X_test = torch.rand(10,1)
+print(model.parameters)
+print(f"Real outcome : {X_test**2}")
+print(f"Predicted output : {model(X_test)}")
+
+
